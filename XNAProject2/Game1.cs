68d051de20d2fa37a8b3d693511e13b5e895d99f
Vadim Solonukhin <jammik94@gmail.com>
2014-05-17 17:56:00 +0400

    Create project

---
+++ XNAProject2/Game1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNAProject2
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        int x=10;
+        int y=10;
+        int width=256;
+        int height=256;       
+        Texture2D pic_texture;
+    
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            pic_texture = Texture2D.FromStream(GraphicsDevice, TitleContainer.OpenStream(@"performer.png"));
+        }
+        protected override void UnloadContent()
+        {
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+            spriteBatch.Begin();
+            spriteBatch.Draw(pic_texture, new Rectangle(x, y, width, height), Color.White);
+            spriteBatch.End();
+           
+        }
+    }
+}
---
+++ XNAProject2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace XNAProject2
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+
+}
